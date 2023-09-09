a1b095b7c84c465a3e978e6e2f939ab65883e9dd
oyindamola <mustaphaamidatoyindamola@gmail.com>
2023-09-09 05:32:46 +0000

    fsh kjdy

--- index.js
+++ index.js
@@ -2,32 +2,26 @@ const express = require("express")
 
 const app = express()
 
-// const currentTime = new Date()
-// const refUtcTime = new Date()
-
-// refUtcTime.setMinutes(refUtcTime.getMinutes() - 2)
-// const timeDiff = currentTime - refUtcTime
-
-// console.log(Math.abs(currentTime));
-
-// if ( Math.abs(currentTime) <=  120000 ) {
-
-// }
 
 app.get("/", (req, res) => {
 
-    res.status(200).json({
-        name: "fbyd fhgifu fhyriedkd",
-        status_code: 200
-    })
-})
+    const currentTime = new Date()
+    const refUtcTime = new Date()
 
-app.get("/home", (req, res) => {
+    refUtcTime.setMinutes(refUtcTime.getMinutes() - 2)
+    const timeDiff = currentTime - refUtcTime
+
+    let showTime
+
+    if ( Math.abs(timeDiff) <=  120000 ) {
+        showTime = currentTime
+    }
+
 
     res.status(200).json({
         slack_name: req.query.slack_name,
         current_day: (new Date).getDay(),
-        utc_time: "",
+        utc_time: showTime,
         track: req.query.track,
         github_file_url: "https://github.com/Amidsts/HNG10_task1/blob/main/index.js",
         github_repo_url: "https://github.com/Amidsts/HNG10_task1",
