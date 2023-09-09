83c457e844bd50dcfa971982bb7f98cc0eed22d9
oyindamola <mustaphaamidatoyindamola@gmail.com>
2023-09-09 06:41:18 +0000

    fsh kjdy

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require("express")
 
 const app = express()
 
+const xTime = new Date();
+xTime.setMilliseconds(0)
+console.log((xTime.toString()));
 
 app.get("/api", (req, res) => {
+
+    let weekDays = [
+        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+    ]
 
     const currentTime = new Date()
     const refUtcTime = new Date()
@@ -20,7 +27,7 @@ app.get("/api", (req, res) => {
 
     res.status(200).json({
         slack_name: req.query.slack_name,
-        current_day: (new Date).getDay(),
+        current_day: weekDays[(new Date).getDay()],
         utc_time: showTime,
         track: req.query.track,
         github_file_url: "https://github.com/Amidsts/HNG10_task1/blob/main/index.js",
